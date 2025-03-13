4e1c88d70ad77493a5f8b27d0c63d7832eee0b53
btahir89 <bilal.tahir89@gmail.com>
2025-03-12 20:18:51 -0700

    new posts

--- components/mdx-layout.tsx
+++ components/mdx-layout.tsx
@@ -8,18 +8,22 @@ interface MdxLayoutProps {
 
 export default function MdxLayout({ children }: MdxLayoutProps) {
   return (
-    <div className="container mx-auto px-4 py-24">
-      <Link 
-        href="/blog"
-        className="inline-flex items-center text-sm text-foreground/60 hover:text-foreground mb-8"
-      >
-        <ArrowLeft className="mr-2 size-4" />
-        Back to Blog
-      </Link>
-      
-      <article className="prose prose-yellow max-w-3xl mx-auto">
-        {children}
-      </article>
+    <div>
+      <div className='px-4 py-8 bg-yellow-50'>
+        <Link
+          href='/blog'
+          className='inline-flex items-center text-sm font-medium text-red-900 hover:text-red-800'
+        >
+          <ArrowLeft className='mr-2 size-4' />
+          Back to Blog
+        </Link>
+      </div>
+
+      <div className='container mx-auto px-4 pb-24'>
+        <article className='prose prose-yellow max-w-3xl mx-auto'>
+          {children}
+        </article>
+      </div>
     </div>
   )
-} 
+}
