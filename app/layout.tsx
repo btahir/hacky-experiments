cf8d960ae7aac5be4c4c6aca5f709393169aac86
btahir89 <bilal.tahir89@gmail.com>
2025-03-17 22:56:54 -0700

    add prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,11 +27,9 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
+    <html lang="en">
       <Analytics />
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen`}>
         {children}
       </body>
     </html>
