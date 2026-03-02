a00f42a26a76163e3e0516bf1e25e26ad11a760b
btahir <bilal.tahir89@gmail.com>
2026-03-01 22:20:46 -0800

    update redis url

--- lib/redis.ts
+++ lib/redis.ts
@@ -2,8 +2,8 @@ import { Redis } from '@upstash/redis'
 import { Ratelimit } from '@upstash/ratelimit'
 
 export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL || '',
-  token: process.env.UPSTASH_REDIS_REST_TOKEN || '',
+  url: process.env.UPSTASH_REDIS_URL || '',
+  token: process.env.UPSTASH_REDIS_TOKEN || '',
 })
 
 // Search: Configurable requests per minute
